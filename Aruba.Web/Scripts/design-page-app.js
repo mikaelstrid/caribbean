1dcae28d5937e531e2050e5eec57aaefa5bfe099
Mikael Strid <mikael.strid@outlook.com>
2015-09-17 13:13:01 +0200

    #156: Resize av iframe.

--- Aruba.Web/Scripts/design-page-app.js
+++ Aruba.Web/Scripts/design-page-app.js
@@ -13,7 +13,7 @@
             }
         }
     })
-    .controller("printEditor", function ($scope, printService, pageService) {
+    .controller("printEditorCtrl", function ($scope, printService, pageService) {
         printService.getPages($scope.printId)
             .then(function (response) {
                 console.log(response.data);
@@ -24,18 +24,36 @@
                 alert("Call printService.getPages failed.");
             });
 
+        $(window).resize(function () {
+            if ($scope.currentPage)
+                $scope.resizeIframe();
+        });
+
         $scope.switchToPage = function (pageId) {
             pageService.getPage(pageId)
-                .then(function(response) {
+                .then(function (response) {
+                    $scope.currentPage = response.data;
                     var p = response.data;
                     $("#dummy").css("margin-top", p.aspectRatioInPercent + "%");
                     var iframe = $("<iframe src='" + p.previewUrl + "' frameborder='0' scrolling='no' style='width: " + p.width + "px; height: " + p.height + "px;'></iframe>");
                     $("#page-editor").empty().append(iframe);
+                    $scope.resizeIframe();
                 }, function(response) {
                     alert("Call to pageService.getPage failed.");
                 });
         }
+
+        $scope.resizeIframe = function () {
+            var scaleValue = $("#page-editor").width() / $scope.currentPage.width;
+            $("#page-editor iframe").css({
+                "transform-origin": "0 0",
+                "transform": "scale(" + scaleValue + ")"
+            });
+        }
     });
+
+
+// === PRIVATE FUNCTIONS ===
 
 
 window.pixel.designpage = (function () {
@@ -197,13 +215,13 @@ window.pixel.designpage = (function () {
     // === PRIVATE FUNCTIONS ===
     function resizeIframe(iframe, previewWidth, iframeWrapperWidth) { // iframe is a jQuery element
         var scaleValue = iframeWrapperWidth / previewWidth;
-        iframe.css('transform-origin', '0 0');
-        iframe.css('transform', 'scale(' + scaleValue + ')');
+        iframe.css("transform-origin", "0 0");
+        iframe.css("transform", "scale(" + scaleValue + ")");
     }
 
     // === PUBLIC FUNCTIONS ===
     var initPage = function (page, actualTemplateWidth) { // page is a jQuery element
-        var iframe = $('iframe', page);
+        var iframe = $("iframe", page);
         iframe.load(function () {
             //$(".editable-textfield", iframe.contents()).click(viewModel.handleTextFieldClick);
             $(".editable-textfield", iframe.contents()).click(function () { console.log("textfield clicked;") });
@@ -214,40 +232,40 @@ window.pixel.designpage = (function () {
             var editableImageFields = $(".editable-imagefield", iframe.contents());
             editableImageFields.each(function () {
                 var parent = $(this).parent();
-                parent.addClass('editable-imagefield-parent');
-                parent.css('z-index', parseInt($(this).css('z-index')) + 1);
+                parent.addClass("editable-imagefield-parent");
+                parent.css("z-index", parseInt($(this).css("z-index")) + 1);
             });
             //editableImageFields.click(viewModel.handleImageFieldClick);
             editableImageFields.click(function () { console.log("imagefield clicked;") });
             $(".editable-imagefield[data-imagefieldtype='1']", iframe.contents()).each(function () {
-                if ($(this).data('afvid')) {
-                    var initData = $(this).data('init');
+                if ($(this).data("afvid")) {
+                    var initData = $(this).data("init");
                     $("img", $(this)).guillotine({ width: $(this).width(), height: $(this).height(), init: initData });
-                    $("img", $(this)).guillotine('disable');
+                    $("img", $(this)).guillotine("disable");
                 }
             });
             $(".editable-imagefield[data-imagefieldtype='2']", iframe.contents()).each(function () {
-                $(this).parent().css('width', $(this).width());
-                $(this).parent().css('height', $(this).height());
-                $(this).css('width', $(this).width());
-                $(this).css('height', $(this).height());
-                if ($(this).data('afvid')) {
-                    var selectedPictureUrl = $(this).data('imgurl');
-                    var targetImage = $('img', $(this));
-                    targetImage.removeAttr('id');
-                    targetImage.removeAttr('width');
-                    targetImage.removeAttr('height');
+                $(this).parent().css("width", $(this).width());
+                $(this).parent().css("height", $(this).height());
+                $(this).css("width", $(this).width());
+                $(this).css("height", $(this).height());
+                if ($(this).data("afvid")) {
+                    var selectedPictureUrl = $(this).data("imgurl");
+                    var targetImage = $("img", $(this));
+                    targetImage.removeAttr("id");
+                    targetImage.removeAttr("width");
+                    targetImage.removeAttr("height");
                     targetImage.hide()
-                        .one('load', function () {
+                        .one("load", function () {
                             var imageField = $(this).parent();
-                            var initData = imageField.data('init');
+                            var initData = imageField.data("init");
                             $(this).guillotine({ width: imageField.width(), height: imageField.height(), init: initData });
-                            $("img", $(this)).guillotine('disable');
+                            $("img", $(this)).guillotine("disable");
                             $(this).show();
                         })
-                        .attr('src', selectedPictureUrl)
+                        .attr("src", selectedPictureUrl)
                         .each(function () {
-                            if (this.complete) $(this).trigger('load');
+                            if (this.complete) $(this).trigger("load");
                         });
                 }
             });
