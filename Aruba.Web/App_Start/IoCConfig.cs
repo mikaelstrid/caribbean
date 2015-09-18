93f9415ab058fd4c552b32d5b37bf4f9b6818df5
Mikael Strid <mikael.strid@outlook.com>
2015-09-18 14:42:19 +0200

    #157: Fixade Autofac/IoC.

--- Aruba.Web/App_Start/IoCConfig.cs
+++ Aruba.Web/App_Start/IoCConfig.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Web.Http;
 using System.Web.Mvc;
 using Autofac;
@@ -13,14 +12,22 @@ namespace Caribbean.Aruba.Web
     {
         public static void RegisterResolver()
         {
+            var assemblies = new[]
+            {
+                Assembly.GetExecutingAssembly(),
+                Assembly.Load("Caribbean.DataAccessLayer.Database"),
+                Assembly.Load("Caribbean.DataAccessLayer.PrintTemplates"),
+                Assembly.Load("Caribbean.DataAccessLayer.RealEstateObjects"),
+            };
+
             var builder = new ContainerBuilder();
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
-            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies()).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces();
-            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies()).Where(t => t.Name.EndsWith("Factory")).AsImplementedInterfaces();
-            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies()).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces();
-            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies()).Where(t => t.Name.EndsWith("Parser")).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(assemblies).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(assemblies).Where(t => t.Name.EndsWith("Factory")).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(assemblies).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces();
+            builder.RegisterAssemblyTypes(assemblies).Where(t => t.Name.EndsWith("Parser")).AsImplementedInterfaces();
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
             GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
